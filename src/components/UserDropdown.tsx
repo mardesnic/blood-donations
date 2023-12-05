acf4ea693e452f7663faac1a814e5bc561d95990
Marin Desnica <mardesnic@users.noreply.github.com>
2023-12-05 21:18:53 +0100

    refactor

--- src/components/UserDropdown.tsx
+++ src/components/UserDropdown.tsx
@@ -2,14 +2,13 @@
 
 import React from 'react';
 import { MdAccountCircle } from 'react-icons/md';
-import { IconButton, Menu, MenuItem, Stack, Typography } from '@mui/material';
+import { IconButton, Menu, MenuItem, Stack } from '@mui/material';
 import { ROUTE_PATHS } from '@/routes';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 export const UserDropdown = () => {
   const router = useRouter();
-  const { data: session } = useSession();
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
@@ -30,12 +29,6 @@ export const UserDropdown = () => {
   return (
     <div>
       <Stack direction='row' alignItems='center' onClick={handleMenu}>
-        <Typography
-          display={{ xs: 'none', sm: 'inline' }}
-          sx={{ cursor: 'pointer' }}
-        >
-          {session?.user?.email}
-        </Typography>
         <IconButton size='large' color='inherit'>
           <MdAccountCircle />
         </IconButton>
