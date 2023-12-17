40336ab9fcc4e6cdc242dfbd162573d61025c585
Marin Desnica <mardesnic@users.noreply.github.com>
2023-12-17 15:46:03 +0100

    fix login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from 'next';
 import { generatePageTitle } from '@/lib/utils';
 import { LoginForm } from '@/app/login/form';
-import { Stack, Typography } from '@mui/material';
+import { Container, Stack, Typography } from '@mui/material';
 
 export const metadata: Metadata = {
   title: generatePageTitle('Login'),
@@ -9,9 +9,11 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <Stack alignItems='center' gap={1} sx={{ marginTop: 8 }}>
-      <Typography variant='h1'>Sign in</Typography>
-      <LoginForm />
-    </Stack>
+    <Container component='main' maxWidth='xl'>
+      <Stack alignItems='center' gap={1} sx={{ marginTop: 8 }}>
+        <Typography variant='h1'>Sign in</Typography>
+        <LoginForm />
+      </Stack>
+    </Container>
   );
 }
