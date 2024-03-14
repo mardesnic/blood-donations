ca7be4b6f1d22a147a1a4420a28d5979aa26df09
Marin Desnica <mardesnic@users.noreply.github.com>
2024-03-14 22:55:23 +0100

    wip

--- src/app/(pages)/donations/[id]/body.tsx
+++ src/app/(pages)/donations/[id]/body.tsx
@@ -2,11 +2,15 @@
 
 import { Card } from '@mui/material';
 import { DonationCreateUpdateForm } from '../forms/createUpdate';
+import { DonationWithDonor } from '@/context/DonationsContext';
+interface Props {
+  donation: DonationWithDonor;
+}
 
-export default function UpdateDonationBody() {
+export default function UpdateDonationBody({ donation }: Props) {
   return (
     <Card sx={{ mt: '30px', p: '30px 24px' }} raised={true}>
-      <DonationCreateUpdateForm />
+      <DonationCreateUpdateForm donation={donation} />
     </Card>
   );
 }
